ff48d957eaff3f29e1434166d9bd56bf39c8ed04
GuiCoelho <guiminervino@gmail.com>
2021-04-15 00:20:03 -0300

    Rabiscos

--- src/pages/Activities/index.js
+++ src/pages/Activities/index.js
@@ -1,13 +1,45 @@
 import React from "react";
 
-import { Container } from "./styles";
+
+import { Container, RectangularButton, Box } from "./styles";
+import Theme from "../../assets/styles/ColorsProvider"
+
+
+
 
 const Activities = () => {
   return (
-    <Container>
-      <h1>UnBom Planner</h1>
-      <h1>Atividades</h1>
-    </Container>
+    <Theme>
+
+      <Container>
+        <nav >
+          <p className="subtitle">Atividades</p>
+        </nav>
+
+
+        <Box className="atividades-container">
+          {/* Atividade 1 */}
+
+          {/* Atividade 2 */}
+          <p> Tarefa 1</p>
+          <p> Tarefa 1</p>
+          <p> Tarefa 1</p>
+          <p> Tarefa 1</p>
+          <RectangularButton>+ Adicionar Atividade</RectangularButton>
+        </Box>
+        <Box className="atividades-container">
+          {/* Atividade 1 */}
+          {/* Atividade 2 */}
+          <p> Tarefa 1</p>
+          <p> Tarefa 1</p>
+          <p> Tarefa 1</p>
+          <p> Tarefa 1</p>
+          <RectangularButton>Adicionar Atividade</RectangularButton>
+        </Box>
+
+      </Container>
+    </Theme>
+
   );
 };
 
--- src/pages/Activities/styles.js
+++ src/pages/Activities/styles.js
@@ -1,3 +1,43 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import ColorsProvider from "../../assets/styles/ColorsProvider"
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  padding: 1rem;
+  display: grid;
+  grid-template-rows: 1fr 3fr 3fr;
+  justify-items: center;
+
+  .atividades-container {
+    display: grid;
+    
+  }
+
+`;
+
+export const Box = styled.article`
+
+ border-radius: 10px;
+
+`;
+
+export const RectangularButton = styled(Link)`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+
+  background-color: ${props => props.theme.darkBrown};
+
+  box-shadow: 6px 6px 16px ,
+    -6px -6px 16px rgba(255, 254, 252, 0.56);
+
+
+    &.disabled {
+    opacity: 0.7;
+  }
+
+  &:not(.disabled):active {
+    box-shadow: 4px 4px 5px rgba(200, 162, 124, 0.32),
+      -4px -4px 5px rgba(255, 254, 252, 0.56);
+  }
+`;
