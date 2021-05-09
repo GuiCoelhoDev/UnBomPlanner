9c15b6dcb740497f0a0e1a947636eaa4bb945ecd
JLYamin <joaol@novaweb.mobi>
2021-05-09 01:26:48 -0300

    WIP: Activity

--- src/pages/ShowActivity/index.js
+++ src/pages/ShowActivity/index.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container, ContentContainer } from "./styles";
 
 import { Link } from "react-router-dom";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import EditIcon from "@material-ui/icons/Edit";
-import EventAvailableIcon from "@material-ui/icons/EventAvailable";
-import BookmarksIcon from "@material-ui/icons/Bookmarks";
-import LocalOfferIcon from "@material-ui/icons/LocalOffer";
-import AssignmentIcon from "@material-ui/icons/Assignment";
-import DateRangeIcon from "@material-ui/icons/DateRange";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+import { ReactComponent as SubmissionDateIcon } from "../../assets/SubmissionDate.svg";
+import { ReactComponent as RealizationDateIcon } from "../../assets/RealizationDate.svg";
+import { ReactComponent as AssociatedDisciplineIcon } from "../../assets/AssociatedDiscipline.svg";
+import { ReactComponent as ActivityTypeIcon } from "../../assets/ActivityType.svg";
+import { ReactComponent as DescriptionIcon } from "../../assets/Description.svg";
 
 function ShowActivity() {
+  const disciplines = ["Nenhuma", "OAC", "IHC", "CN"];
+
+  const [submission, setSubmission] = useState("");
+  const [realization, setRealization] = useState("");
+
+  const formatInputDate = (value) => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d{2})/, "$1/$2")
+      .replace(/(\/\d{2})(\d{2})/, "$1/$2")
+      .replace(/\/(\d{2})(\d{2})\d?$/, " às $1:$2");
+  };
+
   return (
     <Container>
       <nav className="page-header">
-        <Link to="/" className="back-link">
+        <Link to="/activities" className="back-link">
           <ArrowBackIcon />
           Voltar
         </Link>
@@ -26,28 +42,54 @@ function ShowActivity() {
           <h3 className="box-title">Trabalho de OAC</h3>
           <EditIcon />
         </header>
-        <>
+        <div className="inputs-container">
           <div className="input-group">
-            <label>
-              <EventAvailableIcon /> Data de Entrega:
+            <label className="smaller-text">
+              <SubmissionDateIcon /> Data de Entrega:
             </label>
-            <input type="text" />
+            <input
+              type="text"
+              value={submission}
+              onChange={(e) => setSubmission(formatInputDate(e.target.value))}
+              placeholder="A definir"
+            />
           </div>
           <div className="input-group">
-            <label>
-              <DateRangeIcon /> Data de Realização:
+            <label className="smaller-text">
+              <RealizationDateIcon /> Data de Realização:
             </label>
-            <input type="text" />
+            <input
+              type="text"
+              value={realization}
+              onChange={(e) => setRealization(formatInputDate(e.target.value))}
+              placeholder="A definir"
+            />
           </div>
-        </>
-        <>
+        </div>
+        <div className="inputs-container">
           <div className="input-group">
-            <label>
-              <BookmarksIcon /> Disciplina Associada:
+            <label className="smaller-text">
+              <AssociatedDisciplineIcon /> Disciplina Associada:
             </label>
-            <input type="text" />
+            <Dropdown
+              controlClassName="select"
+              value={"Nenhuma "}
+              options={disciplines}
+            />
           </div>
-        </>
+          <div className="input-group horiz-group">
+            <label className="smaller-text">
+              <ActivityTypeIcon /> Tipo de Atividade:
+            </label>
+            <input type="text" placeholder="A definir" />
+          </div>
+        </div>
+        <div className="textarea-group">
+          <label className="smaller-text">
+            <DescriptionIcon /> Descrição:
+          </label>
+          <textarea />
+        </div>
       </ContentContainer>
     </Container>
   );
--- src/pages/ShowActivity/styles.js
+++ src/pages/ShowActivity/styles.js
@@ -13,11 +13,12 @@ export const ContentContainer = styled.article`
   max-height: 75vh;
   overflow-y: auto;
 
-  width: 90%;
+  width: 95%;
   padding: 1.2rem 1rem;
 
   display: grid;
   grid-template-rows: repeat(3, min-content) 1fr;
+  gap: 1rem;
 
   header {
     display: grid;
@@ -31,6 +32,83 @@ export const ContentContainer = styled.article`
       color: ${({ theme }) => theme.darkBrown};
       width: 1.4rem;
       height: 1.4rem;
+    }
+  }
+
+  .input-group,
+  .textarea-group {
+    label {
+      display: grid;
+      grid-template-columns: min-content min-content;
+      align-items: center;
+      gap: 0.25rem;
+      white-space: nowrap;
+      svg {
+        height: 1rem;
+        width: 1rem;
+        color: ${({ theme }) => theme.darkBrown};
+      }
+    }
+  }
+
+  .inputs-container {
+    display: grid;
+    gap: 0.5rem;
+
+    margin-bottom: 0.5rem;
+
+    .input-group {
+      display: grid;
+      grid-template-columns: min-content min-content;
+      justify-content: space-between;
+
+      input,
+      .select {
+        width: 5.5rem;
+        height: 1.2rem;
+        font-size: 0.8rem;
+        background-color: #faf3ec;
+        padding: 0;
+        border-radius: 2px;
+        border: none;
+        color: ${({ theme }) => theme.darkBrown};
+
+        text-align: center;
+
+        &::placeholder {
+          color: ${({ theme }) => theme.clearBrown};
+          font-weight: 300;
+        }
+
+        &:focus {
+          outline: none;
+          border: 1px solid #ffe7ce;
+        }
+      }
+
+      .select {
+        .Dropdown-arrow {
+          display: none;
+        }
+
+        .Dropdown-placeholder {
+          padding-top: 0.1rem;
+        }
+      }
+      .Dropdown-menu {
+        font-size: 0.8rem;
+        border: none;
+        border-top: 1px solid #ffe7ce;
+
+        .Dropdown-option {
+          color: ${({ theme }) => theme.darkBrown};
+          background-color: #faf3ec;
+
+          &.is-selected {
+            background-color: #faeada;
+          }
+        }
+      }
     }
   }
 `;
