267040d7923693c586f055668e835e5c6cc2e45e
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-05-09 23:34:58 -0300

    :sparkles: feat: adds crud of activity
    created a services file with the CRUD methods for activities

--- src/pages/Activities/index.js
+++ src/pages/Activities/index.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-
-import { Container, ListItem, ActivitiesContainer } from "./styles";
+import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
@@ -8,8 +6,34 @@ import FilterListIcon from "@material-ui/icons/FilterList";
 import CheckIcon from "@material-ui/icons/Check";
 import AddIcon from "@material-ui/icons/Add";
 
+import { index, create } from "../../services/Activities"
+
+import { Container, ListItem, ActivitiesContainer } from "./styles";
+
 const Activities = () => {
+
+  const [activities, setActivities] = useState([]);
   const [checkedBox, setCheckedBox] = useState(false);
+
+  const fetchActivities = async () => {
+    const response = await index();
+    setActivities(response);
+  }
+
+  useEffect(() => {
+    fetchActivities();
+  }, []
+  );
+
+  const createActivity = async () => {
+    try {
+      await create({name: 'Estudo'});
+      await fetchActivities();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <Container>
       <nav className="page-header">
@@ -30,24 +54,24 @@ const Activities = () => {
         <div className="activities-box">
           <div className="list-container">
             <ul>
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              {activities?.map((i) => (
                 <ListItem
                   onClick={() => setCheckedBox((c) => !c)}
                   checked={checkedBox}
-                  key={i}
+                  key={i.id}
                 >
                   <div className="checkbox">
                     <CheckIcon />
                   </div>
                   <span className="name smaller-text">
-                    Montar telas no Figma
+                    {i.value.name}
                   </span>
                   <div className="tag">IHC</div>
                 </ListItem>
               ))}
             </ul>
           </div>
-          <div className="add-activity">
+          <div className="add-activity" onClick={() => createActivity()}>
             <AddIcon /> Adicionar Nova Atividade
           </div>
         </div>
